fae6706dafc02607ecdfebc3e2026a8278f58e99
Agata Gołaszewska <agata@gda.pl>
2018-08-28 15:51:14 +0200

    the first commit

---
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseMethod_BasicCalculatorTODO/AkSpringExerciseMethodBasicCalculatorApplicationTODO.java
@@ -0,0 +1,19 @@
+package pl.akademiakodu.AK_Spring_ExerciseMethod_BasicCalculatorTODO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot
+ * Projekt z Kursu: Java Bootcamp ze szkoły Akademia Kodu
+ * Cel edukacyjny
+ *
+ */
+
+@SpringBootApplication
+public class AkSpringExerciseMethodBasicCalculatorApplicationTODO {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkSpringExerciseMethodBasicCalculatorApplicationTODO.class, args);
+	}
+}
